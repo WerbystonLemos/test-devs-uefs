e08226bd60d46cf9891cb26203c761235202976f
Werbyston Lemos <werbyston@hotmail.com>
2025-03-21 02:31:53 -0300

    [Feature] conclusão inicial do CRUD de User

--- avansysTest/app/Http/Controllers/UserController.php
+++ avansysTest/app/Http/Controllers/UserController.php
@@ -12,7 +12,7 @@ class UserController extends Controller
      */
     public function index()
     {
-        return User::all()->toJson();
+        return User::all();
     }
 
     /**
@@ -28,7 +28,28 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validateData = $request->validate([
+            'name'       => 'required|string',
+            'email'      => 'required|email',
+            'date_birth' => 'required|date',
+        ]);
+
+        try
+        {
+            User::create($validateData);
+
+            return response()->json([
+                'message' => 'Usuário criado com sucesso!',
+                'data'    => $validateData,
+            ],200);
+        }
+        catch (\Exception $erro)
+        {
+            return response()->json([
+                'message' => 'Erro ao criar usuário!',
+                'error'   => $erro->getMessage(),
+            ], 500);
+        }
     }
 
     /**
@@ -52,7 +73,39 @@ class UserController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $validateData = $request->validate([
+            'name'          => 'string',
+            'email'         => 'email',
+            'date_birth'    => 'date',
+        ]);
+
+        try
+        {
+            $user = User::find($id);
+
+            if($user)
+            {
+                $user->update($validateData);
+
+                return response()->json([
+                    'message'   => 'Usuário atualizado com sucesso!',
+                    'data'      => $validateData,
+                ], 200);
+            }
+            else
+            {
+                return response()->json([
+                    'message'   => 'Usuário não encontrado!',
+                ], 404);
+            }
+        }
+        catch (\Exception $erro)
+        {
+            return response()->json([
+                'message'   => 'Erro ao atualizar usuário!',
+                'error'     => $erro->getMessage(),
+            ], 500);
+        }
     }
 
     /**
@@ -60,6 +113,19 @@ class UserController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $deleted  = (User::destroy($id));
+
+        if($deleted)
+        {
+            return response()->json([
+                'message'   => 'Usuário deletado com sucesso!',
+            ], 200);
+        }
+        else
+        {
+            return response()->json([
+                'message'   => 'Usuário não encontrado!',
+            ], 404);
+        }
     }
 }
