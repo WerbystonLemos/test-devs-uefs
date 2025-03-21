b055c9f869856944de9f79e5c78160caa15d0cc0
Werbyston Lemos <werbyston@hotmail.com>
2025-03-21 00:07:27 -0300

    [Build] building do projeto

---
+++ avansysTest/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -e
+echo "Aguardando Banco de Dados..."
+while ! mysqladmin ping -h"$DB_HOST" --silent; do
+    sleep 2
+done
+echo "Banco de Dados está pronto!"
+if [ ! -f ".env" ]; then
+    cp .env.example .env
+fi
+php artisan key:generate
+php artisan migrate --force
+php artisan db::seed
+chown -R www-data:www-data /var/www/storage /var/www/bootstrap/cache
+exec "$@"
