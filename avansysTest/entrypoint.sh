6fead6b352e50a7e905d71c0bf30b1e288600ee0
Werbyston Lemos <werbyston@hotmail.com>
2025-03-23 05:25:46 -0300

    [Fix] conserto na imagem dcker

--- avansysTest/entrypoint.sh
+++ avansysTest/entrypoint.sh
@@ -5,11 +5,14 @@ while ! mysqladmin ping -h"$DB_HOST" --silent; do
     sleep 2
 done
 echo "Banco de Dados está pronto!"
+if [ ! -d "vendor" ]; then
+    echo "Instalando dependências do Composer..."
+    composer install --no-dev --optimize-autoloader
+fi
 if [ ! -f ".env" ]; then
     cp .env.example .env
 fi
 php artisan key:generate
-php artisan migrate --force
-php artisan migrate:fresh --seed
+php artisan migrate --force && php artisan db:seed --force
 chown -R www-data:www-data /var/www/storage /var/www/bootstrap/cache
 exec "$@"
